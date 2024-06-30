d17419cb0fc95a18e6898cf3e8958f2e5111bf06
gerry <gerrydesrian488@gmail.com>
2024-06-30 12:08:57 +0700

    limit payload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
