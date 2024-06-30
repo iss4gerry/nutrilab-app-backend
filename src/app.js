aff52ae4cf533cb4bbe4a5976e37422b4c3af69e
gerry <gerrydesrian488@gmail.com>
2024-06-30 12:09:50 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
+app.use(cors())
+app.use('*', cors())
 
 app.get('/', (req, res) => {
     res.send('Hello World')
