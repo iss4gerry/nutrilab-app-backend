a828d014dcac253742e054425e936a0a37277451
gerry <gerrydesrian488@gmail.com>
2024-07-01 16:44:57 +0700

    morgan

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const errorHandler = require('./middlewares/error')
 const cors = require('cors')
+const morgan = require('morgan')
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cors())
 app.use('*', cors())
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
