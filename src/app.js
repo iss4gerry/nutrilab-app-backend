1cbecedbe7f7f5532a91ccd5b84d1751e82dd8a1
gerry <gerrydesrian488@gmail.com>
2024-07-01 16:33:11 +0700

    error handler

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const errorHandler = require('./middlewares/error')
 const cors = require('cors')
 
 app.use(express.json({ limit: '10mb' }))
@@ -10,5 +11,7 @@ app.use('*', cors())
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+app.use(errorHandler)
 
 module.exports = app
