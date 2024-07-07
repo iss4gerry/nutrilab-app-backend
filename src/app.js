6d56cbdd41c2c7e44c7e05ed244da8a369cf7d72
gerry <gerrydesrian488@gmail.com>
2024-07-07 11:02:02 +0700

    custom swagger css

--- src/app.js
+++ src/app.js
@@ -16,7 +16,11 @@ app.use(cors())
 app.use('*', cors())
 app.use(morgan('dev'))
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World')
