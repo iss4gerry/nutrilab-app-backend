777022655b9fb2bbd4dee8c7e6400789d46c4ae8
gerry <gerrydesrian488@gmail.com>
2024-07-08 11:36:59 +0700

    error middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,13 +1,13 @@
 const httpStatus = require('http-status');
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR
-  const message = err.message || 'Internal Server Error'
+    const statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR
+    const message = err.message || 'Internal Server Error'
 
-  res.status(statusCode).json({
-    status: statusCode,
-    message: message,
-  })
+    res.status(statusCode).json({
+      status: statusCode,
+      message: message,
+   })
 }
 
 module.exports = errorHandler
